8e9f87634f7906fd43501c036aeb2ec97dfbf7e3
TheoMutel <mtt3995a@etu.iut-tlse3.fr>
2024-12-03 14:55:44 +0100

    Exercice 2 terminer

--- app/src/main/java/com/example/monprofil/Main.kt
+++ app/src/main/java/com/example/monprofil/Main.kt
@@ -1,6 +1,7 @@
 package com.example.monprofil
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import java.util.Locale.filter
 
 open class Piece(val largeur: Float, val longueur: Float, val nom: String){
     fun surface(): Int {
@@ -10,7 +11,7 @@ open class Piece(val largeur: Float, val longueur: Float, val nom: String){
         return result;
     }
 }
-
+/*
 class Cuisine(largeur: Float, longueur: Float, nom: String) : Piece(largeur, longueur, nom){
 
 }
@@ -24,6 +25,23 @@ val list = MutableStateFlow<List<Cuisine>>(emptyList())
 
 val list2 = MutableStateFlow<List<Salon>>(emptyList())
 
-fun parcours(){
+fun parcourslist(){
 
+}*/
+
+class Etudiant( val name: String, val promo: String, val matieres: List<String>)
+
+val etudiants = listOf(
+    Etudiant("Paul", "2025", listOf("mobile","web", "BDD")),
+    Etudiant("Yazid","2024", listOf("mobile","Android","Réseau")),
+    Etudiant("Caroline","2025", listOf("SE","Anglais")),
+)
+
+
+
+
+fun main(){
+    etudiants.filter { it.promo == "épice"}.forEach { println(it.name) }
+    etudiants.groupBy { it.promo }.forEach { k,v -> println("clé: $k, ${v.map { it.name}}") }
+   /* println(etudiants.sumOf { it.matieres })*/
 }
